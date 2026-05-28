796b4634824870b97296f8e639316fa7c63d7c2d
yashh972 <yashjadhav0207@gmail.com>
2026-05-28 17:40:53 +0530

    Configure outputFileTracingIncludes to force bundle pdf-parse worker files on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   serverExternalPackages: ['pdf-parse'],
+  outputFileTracingIncludes: {
+    '/api/quizzes': ['./node_modules/pdf-parse/**/*'],
+    '/api/flashcards': ['./node_modules/pdf-parse/**/*'],
+  },
 };
 
 export default nextConfig;
